56917bc873757a508d24b28c9a217d05512d81a4
KM <kampanartm.careers@gmail.com>
2024-11-15 10:55:23 +1100

    update the modprobe to fix bridge module missing

--- kubeadm-clusters/virtualbox/ubuntu/setup-kernel.sh
+++ kubeadm-clusters/virtualbox/ubuntu/setup-kernel.sh
@@ -3,8 +3,9 @@
 # Sets up the kernel with the requirements for running Kubernetes
 set -e
 
-# Add br_netfilter kernel module
-echo "br_netfilter" >> /etc/modules
+# Add br_netfilter kernel module otherwise it may face this error due to the bridge module missing its directory:
+# "Failed to check br_netfilter: stat /proc/sys/net/bridge/bridge-nf-call-iptables: no such file or directory"
+modprobe br_netfilter
 
 # Set network tunables
 cat <<EOF >> /etc/sysctl.d/10-kubernetes.conf
@@ -12,3 +13,4 @@ net.bridge.bridge-nf-call-iptables=1
 net.ipv4.ip_forward=1
 EOF
 
+systemctl restart systemd-sysctl.service
